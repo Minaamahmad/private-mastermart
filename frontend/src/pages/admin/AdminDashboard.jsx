8e3c94540f9b9bc253c23c9e22c67653f7ac8960
MinaamAhmad <minaamahmad@gmail.com>
2026-01-04 02:04:18 +0500

    image loading

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -12,27 +12,26 @@ const AdminDashboard = () => {
       <div className="admin-dashboard-grid">
         <Link to="/admin/products" className="admin-dashboard-card admin-card-products">
           <div className="admin-card-icon">
-            <img src='../../Assets/clipboard-svgrepo-com.svg' alt='Manage Products' />
+            <img src='/Assets/clipboard-svgrepo-com.svg' alt='Manage Products' />
           </div>
           <div className="admin-card-content">
             <h2>Manage Products</h2>
             <p>Add, edit, or delete products from your store</p>
           </div>
           <div className="admin-card-arrow">
-            <img src='../../Assets/right-arrow-svgrepo-com.svg' alt="arrow right" />
+            <img src='/Assets/right-arrow-svgrepo-com.svg' alt="arrow right" />
           </div>
         </Link>
         <Link to="/admin/orders" className="admin-dashboard-card admin-card-orders">
           <div className="admin-card-icon">
-            <img src='../../Assets/shopping-bag-4-svgrepo-com.svg' alt='Manage Orders' />
+            <img src='/Assets/shopping-bag-4-svgrepo-com.svg' alt='Manage Orders' />
           </div>
           <div className="admin-card-content">
             <h2>Manage Orders</h2>
             <p>View and update order status for customer orders</p>
           </div>
           <div className="admin-card-arrow">
-          <img src='../../Assets/right-arrow-svgrepo-com.svg' alt="arrow right" />
-
+            <img src='/Assets/right-arrow-svgrepo-com.svg' alt="arrow right" />
           </div>
         </Link>
       </div>
